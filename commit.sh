0aee9efecb3d83afa542afe969476b13f2fc1010
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-12-04 11:41:56 +0300

    2023-12-04-11-41-56

--- commit.sh
+++ commit.sh
@@ -4,7 +4,10 @@
 
 if [ -n "$(git status -s)" ]
 then
+    git status 
     git add .
     git commit -m "$(date +"%Y-%m-%d-%H-%M-%S")"
     git push 
+else
+    echo "nothing to commit in repo"
 fi
