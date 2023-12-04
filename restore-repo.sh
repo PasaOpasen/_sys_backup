a02a5c9eadbe6c5ef99f60a13d2f7bc0b5d351eb
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-12-04 21:23:49 +0300

    -

--- restore-repo.sh
+++ restore-repo.sh
@@ -5,28 +5,44 @@
 
 set -e
 
-read -s -p 'Write current sudo password: ' passwd
+if [ -f 'password.txt' ] && $(cat password.txt | sudo -S -v )
+then
+    echo "Found password file"
+    passwd="$(cat password.txt)"
+else
+    read -s -p 'Write current sudo password: ' passwd
 
-echo $passwd | sudo -S -v && echo "OK"
+    echo $passwd | sudo -S -v && echo "OK"
+
+fi
 
 echo $passwd | sudo -S dnf install -y openssh git python3 python3-pip make
 
-ssh-keygen || true
+if [ "$(git config --get remote.origin.url)" == 'git@github.com:PasaOpasen/_sys_backup.git' ]
+then 
+    echo "Found exising repository, update..."
+    git pull
 
-read -n 1 -p 'Copy ssh keys to GitHub MANUALLY and press any key...'
+else
 
-repo_path=~/sys-backup
+    ssh-keygen || true
 
-read -p "Write path to backup repo (${repo_path}): " rp
+    read -n 1 -p 'Copy ssh keys to GitHub MANUALLY and press any key...'
 
-if [ -n "$rp" ]
-then
-    repo_path=$rp
+    repo_path=~/sys-backup
+
+    read -p "Write path to backup repo (${repo_path}): " rp
+
+    if [ -n "$rp" ]
+    then
+        repo_path=$rp
+    fi
+
+    git clone https://github.com/PasaOpasen/_sys_backup ${repo_path}
+
+    cd ${repo_path}
+
 fi
-
-git clone https://github.com/PasaOpasen/_sys_backup ${repo_path}
-
-cd ${repo_path}
 
 # save password for ansible
 echo $passwd > password.txt
